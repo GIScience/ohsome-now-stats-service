fd8f728322d4731ccf339fa43e471a579e70c2a5
Matthias Merdes <matthias.merdes@heigit.org>
2023-03-16 16:59:01 +0100

    test: simplify dummy test  #1
    ohsome-contributions-stats-service: 'Set up project with Spring Boot'
    https://github.com/GIScience/ohsome-contributions-stats-service/issues/1

--- src/test/kotlin/org/heigit/ohsome/stats/StatsApplicationTests.kt
+++ src/test/kotlin/org/heigit/ohsome/stats/StatsApplicationTests.kt
@@ -1,13 +1,11 @@
 package org.heigit.ohsome.stats
 
 import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
-class StatsApplicationTests {
 
-	@Test
-	fun contextLoads() {
-	}
+class SmokeTests {
+
+    @Test
+    fun run() = Unit
 
 }
