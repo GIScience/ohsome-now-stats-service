e1406bcf71decae65548efdd657545d846418437
Matthias Merdes <matthias.merdes@heigit.org>
2023-03-16 17:13:29 +0100

    feat: add static start page, disable jdbc starter for now  #1
    ohsome-contributions-stats-service: 'Set up project with Spring Boot'
    https://github.com/GIScience/ohsome-contributions-stats-service/issues/1

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,9 +17,11 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
-	implementation("org.springframework.boot:spring-boot-starter-jdbc")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+
+//	implementation("org.springframework.boot:spring-boot-starter-jdbc")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 tasks.withType<KotlinCompile> {
