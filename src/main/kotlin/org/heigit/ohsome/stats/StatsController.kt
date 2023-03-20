40128f5e75be2dec0c967430c8f3f873f137e6dd
Matthias Merdes <matthias.merdes@heigit.org>
2023-03-20 15:14:29 +0100

    feat: add controller for REST endpoint 'stats' with static data  #2
    The example data was sourced today at 2pm UTC from the existing azure deployment.
    
    ohsome-contributions-stats-service: 'Set up REST endpoint with fixed data'
    https://github.com/GIScience/ohsome-contributions-stats-service/issues/2

---
+++ src/main/kotlin/org/heigit/ohsome/stats/StatsController.kt
@@ -0,0 +1,26 @@
+package org.heigit.ohsome.stats
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+
+// static data taken from http://osm-stats-production-api.azurewebsites.net/stats at 2pm, 20 March 2023
+
+
+@RestController
+class StatsController {
+
+
+    @GetMapping("/stats")
+    fun stats() = mapOf(
+        "changesets" to 65009011,
+        "users" to 3003842,
+        "roads" to 45964973.0494135,
+        "buildings" to 844294167,
+        "edits" to 1095091515,
+        "latest" to "2023-03-20T10:55:38.000Z",
+        "hashtag" to "*"
+    )
+
+
+}
