777e27bbbb8bf953de75617062fec8b56c2974e5
Matthias Merdes <matthias.merdes@heigit.org>
2023-03-27 14:43:39 +0200

    refactor: extract repo from controller #3
    ohsome-contributions-stats-service: 'Connect service to ClickHouse Database'
    https://github.com/GIScience/ohsome-contributions-stats-service/issues/3

---
+++ src/main/kotlin/org/heigit/ohsome/stats/StatsRepo.kt
@@ -0,0 +1,46 @@
+package org.heigit.ohsome.stats
+
+import org.jdbi.v3.core.Handle
+import org.jdbi.v3.core.Jdbi.create
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
+import javax.sql.DataSource
+
+
+@Component
+class StatsRepo {
+
+    //TODO: https://www.baeldung.com/spring-boot-jdbi
+
+
+    @Autowired
+    lateinit var dataSource: DataSource
+
+
+    //language=SQL
+    val sql = """
+        SELECT
+            count(distinct changeset_id) as changesets,
+            count(distinct user_id) as users,
+            sum(road_length) as roads,
+            count(building_area) as buildings,
+            count(*) as edits,
+            FROM_UNIXTIME(intDiv(max(changeset_timestamp), 1000)) as latest
+        FROM "__stats_all_unnested"
+        WHERE
+            hashtag = '#missingmaps';
+        """.trimIndent()
+
+
+    //TODO: add key for hashtag used for query
+    fun getStats() = create(dataSource)
+        .withHandle<Map<String, Any>, RuntimeException>(::asMap)
+
+
+    private fun asMap(handle: Handle) = handle
+        .createQuery(sql)
+        .mapToMap()
+        .single()
+
+
+}
