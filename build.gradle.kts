4301038ccd03be5c25318f70936722f39d4ccadb
Matthias Merdes <matthias.merdes@heigit.org>
2023-04-20 14:27:53 +0200

    test: upgrade boot version and fix formatting   #3
    ohsome-contributions-stats-service: 'Connect service to ClickHouse Database'
    https://github.com/GIScience/ohsome-contributions-stats-service/issues/3

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "3.0.4"
+    id("org.springframework.boot") version "3.0.5"
     id("io.spring.dependency-management") version "1.1.0"
     kotlin("jvm") version "1.8.10"
     kotlin("plugin.spring") version "1.8.10"
@@ -19,7 +19,7 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-jdbc")
 
