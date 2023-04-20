f4e6958b714298610ad4fc5418bfc7becab89f66
Matthias Merdes <matthias.merdes@heigit.org>
2023-04-20 14:56:40 +0200

    test: add test-containers based integration (smoke) test  #3
    ohsome-contributions-stats-service: 'Connect service to ClickHouse Database'
    https://github.com/GIScience/ohsome-contributions-stats-service/issues/3

--- src/test/kotlin/org/heigit/ohsome/stats/StatsRepoIntegrationTests.kt
+++ src/test/kotlin/org/heigit/ohsome/stats/StatsRepoIntegrationTests.kt
@@ -1,27 +1,24 @@
 package org.heigit.ohsome.stats
 
-import org.junit.jupiter.api.Assertions.*
-import org.junit.jupiter.api.Disabled
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import org.testcontainers.containers.ClickHouseContainer
 import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
 
 
-@Disabled("testcontainers not yet working")
 @Testcontainers
 class StatsRepoIntegrationTests {
 
 
     @Container
-    private val clickHouse: ClickHouseContainer = ClickHouseContainer()
+    private val clickHouse: ClickHouseContainer = ClickHouseContainer("clickhouse/clickhouse-server")
 
 
     @Test
     fun `stats should return data from the db repo`() {
 
         assertTrue(clickHouse.isRunning)
-
 
     }
 
