0fdeb42d1650e04755ba064a733046779a053415
Johannes Visintini <johannes.visintini@heigit.org>
2023-04-21 20:21:01 +0200

    feat: re-enable /stats endpoint

--- src/main/kotlin/org/heigit/ohsome/stats/StatsController.kt
+++ src/main/kotlin/org/heigit/ohsome/stats/StatsController.kt
@@ -16,9 +16,9 @@ class StatsController {
 
 
 //    TODO: add spring path variable for 'hashtag'
-//    @GetMapping("/stats")
-//    @Operation(summary = "Returns live data from DB")
-fun statsDB() = this.repo.getStats()
+    @GetMapping("/stats")
+    @Operation(summary = "Returns live data from DB")
+    fun statsDB() = this.repo.getStats()
 
 
     // static data taken from http://osm-stats-production-api.azurewebsites.net/stats at 2pm, 20 March 2023
