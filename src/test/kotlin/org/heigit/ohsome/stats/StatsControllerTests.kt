20b4479a096174f81769855021df258726266b20
Matthias Merdes <matthias.merdes@heigit.org>
2023-04-25 15:21:27 +0200

    test: re-enable disabled test  #3
    ohsome-contributions-stats-service: 'Connect service to ClickHouse Database'
    https://github.com/GIScience/ohsome-contributions-stats-service/issues/3

--- src/test/kotlin/org/heigit/ohsome/stats/StatsControllerTests.kt
+++ src/test/kotlin/org/heigit/ohsome/stats/StatsControllerTests.kt
@@ -1,7 +1,6 @@
 package org.heigit.ohsome.stats
 
 import org.hamcrest.CoreMatchers.containsString
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.mockito.Mockito.`when`
 import org.springframework.beans.factory.annotation.Autowired
@@ -37,7 +36,6 @@ class StatsControllerTests {
       }"""
 
 
-    @Disabled("endpoint currently disabled")
     @Test
     fun `stats should return data from the db repo`() {
 
