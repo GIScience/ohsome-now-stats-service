0b8bb3e479da1bff2dcebe0ea402d231eabd9255
Matthias Merdes <matthias.merdes@heigit.org>
2023-05-04 15:39:41 +0200

    feat: enable cors for all origins with a preflight response cache of 30 minutes  #9
    enabled for all endpoints of stats controller
    
    ohsome-contributions-stats-service: 'Enable CORS'
    https://github.com/GIScience/ohsome-contributions-stats-service/issues/9

--- src/main/kotlin/org/heigit/ohsome/stats/StatsController.kt
+++ src/main/kotlin/org/heigit/ohsome/stats/StatsController.kt
@@ -5,13 +5,11 @@ import io.swagger.v3.oas.annotations.Parameter
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.format.annotation.DateTimeFormat
 import org.springframework.format.annotation.DateTimeFormat.ISO
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import java.time.Instant
 
 
+@CrossOrigin
 @RestController
 class StatsController {
 
