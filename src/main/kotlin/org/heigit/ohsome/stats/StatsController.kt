bcd7e65c22770af85878023f2eef289e6897fe7e
Clemens Langer <clemens.langer@stud.uni-heidelberg.de>
2023-06-15 12:57:39 +0200

    feat: add first Draft of timeseries endpoint #7

--- src/main/kotlin/org/heigit/ohsome/stats/StatsController.kt
+++ src/main/kotlin/org/heigit/ohsome/stats/StatsController.kt
@@ -2,17 +2,20 @@ package org.heigit.ohsome.stats
 
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.Parameter
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.format.annotation.DateTimeFormat
 import org.springframework.format.annotation.DateTimeFormat.ISO
 import org.springframework.web.bind.annotation.*
 import java.time.Instant
+import kotlin.system.measureTimeMillis
 
 
 @CrossOrigin
 @RestController
 class StatsController {
 
+    private val logger = LoggerFactory.getLogger(StatsController::class.java)
 
     @Autowired
     lateinit var repo: StatsRepo
@@ -35,7 +38,6 @@ class StatsController {
         @DateTimeFormat(iso = ISO.DATE_TIME)
         endDate: Instant?
     ): Map<String, Any> {
-
         return this.repo.getStatsForTimeSpan(hashtag, startDate, endDate) +
                 echoRequestParameters(startDate, endDate)
 
@@ -56,19 +58,108 @@ class StatsController {
         return extraMap
     }
 
-
     // static data taken from http://osm-stats-production-api.azurewebsites.net/stats at 2pm, 20 March 2023
     @Operation(summary = "Returns a static snapshot of OSM statistics (for now)")
     @GetMapping("/stats_static")
     fun statsStatic() = mapOf(
-        "changesets" to 65009011,
-        "users" to 3003842,
-        "roads" to 45964973.0494135,
-        "buildings" to 844294167,
-        "edits" to 1095091515,
-        "latest" to "2023-03-20T10:55:38.000Z",
-        "hashtag" to "*"
+            "changesets" to 65009011,
+            "users" to 3003842,
+            "roads" to 45964973.0494135,
+            "buildings" to 844294167,
+            "edits" to 1095091515,
+            "latest" to "2023-03-20T10:55:38.000Z",
+            "hashtag" to "*"
     )
 
+    /**
+     * Retrieves live data from the database aggregated by interval.
+     *
+     * @param hashtag the hashtag to query for - case-insensitive and without the leading '#'
+     * @param startDate the (inclusive) start date for the query in ISO format (e.g. 2020-01-01T00:00:00Z) (optional)
+     * @param endDate the (exclusive) end date for the query in ISO format (e.g. 2020-01-01T00:00:00Z) (optional)
+     * @param interval the granularity defined as Intervals in ISO 8601 time format (e.g. P1M) (optional, defaults to auto)
+     * @return a map containing the response data with aggregated statistics
+     */
+
+    fun listMapToString(list: List<Map<String, Any>>): String {
+        val stringBuilder = StringBuilder()
+
+        for (map in list) {
+            for ((key, value) in map) {
+                stringBuilder.append("$key: $value\n")
+            }
+            stringBuilder.append("\n")
+        }
+
+        return stringBuilder.toString()
+    }
+    @Operation(summary = "Returns live data from DB aggregated by month")
+    @GetMapping("/stats/{hashtag}/interval")
+    fun statsInterval(
+            @Parameter(description = "the hashtag to query for - case-insensitive and without the leading '#'")
+            @PathVariable
+            hashtag: String,
+
+            @Parameter(description = "the (inclusive) start date for the query in ISO format (e.g. 2020-01-01T00:00:00Z)")
+            @RequestParam("startdate", required = false)
+            @DateTimeFormat(iso = ISO.DATE_TIME)
+            startDate: Instant = Instant.ofEpochSecond(0),
+
+            @Parameter(description = "the (exclusive) end date for the query in ISO format (e.g. 2020-01-01T00:00:00Z)")
+            @RequestParam("enddate", required = false)
+            @DateTimeFormat(iso = ISO.DATE_TIME)
+            endDate: Instant = Instant.now(),
+
+            @Parameter(description = "the granularity defined as Intervals in ISO 8601 time format eg: P1M")
+            @RequestParam("interval", required = false,) //defaults to auto
+            interval: String = "auto"
+
+            /*Alternativly
+            @Parameter(description = "the timerange defined start date and enddate for the query in ISO  8601 format (e.g. 2020-01-01T00:00:00Z) and the interval also in the ISO format 8601 ")
+            @RequestParam("timerange", required = true) //is it possible to set the independent parts to diffent defaults?
+            timetrange: String
+
+            //this function is used to split the iso 8601 String into startdate, enddate and interval
+            fun splitISOInterval(isoInterval: String): Triple<Instant, Instant, String> {
+                val parts = isoInterval.split("/")
+                if (parts.size != 3) {
+                    throw IllegalArgumentException("Invalid ISO 8601 interval format.")
+                }
+
+                val start = Instant.parse(parts[0])
+                val end = Instant.parse(parts[1])
+                val duration = parts[2]
+
+                return Triple(start, end, duration)
+            }
+             */
+    ): Map<String, Any> {
+        val response = mutableMapOf<String, Any>()
+
+        val executionTime = measureTimeMillis {
+            val queryResult = this.repo.getStatsForTimeSpanInterval(hashtag, startDate, endDate, interval)
+            logger.info(listMapToString(queryResult))
+            response["result"] = queryResult
+        }
+
+        response["attribution"] = mapOf("url" to "https://ohsome.org/copyrights", "text" to "© OpenStreetMap contributors")
+        response["apiVersion"] = "1.9.0"
+        response["metadata"] = mapOf(
+                "executionTime" to executionTime,
+                "requestUrl" to "https://stats.ohsome.org/..." //ToDo
+        )
+        response["query"] = mapOf(
+                "timespan" to mapOf(
+                        "startDate" to (startDate).toString(),
+                        "endDate" to (endDate).toString(),
+                        "interval" to interval
+                ),
+                "hashtag" to hashtag
+
+        )
+        response["latest"] = Instant.now().toString() //ToDo
+
+        return response
+    }
 
 }
--- src/main/kotlin/org/heigit/ohsome/stats/StatsRepo.kt
+++ src/main/kotlin/org/heigit/ohsome/stats/StatsRepo.kt
@@ -7,8 +7,8 @@ import org.springframework.stereotype.Component
 import java.time.Instant
 import java.time.Instant.EPOCH
 import java.time.Instant.now
+import java.time.Duration
 import javax.sql.DataSource
-
 
 @Component
 class StatsRepo {
@@ -25,7 +25,7 @@ class StatsRepo {
             count(distinct user_id) as users,
             sum(road_length) as roads,
             count(building_area) as buildings,
-            count(*) as edits,
+            count(*) as edits,s
             FROM_UNIXTIME(intDiv(max(changeset_timestamp), 1000)) as latest
         FROM "stats"
         WHERE
@@ -43,27 +43,93 @@ class StatsRepo {
         FROM "stats"
         WHERE
             hashtag = ? and changeset_timestamp > ? and changeset_timestamp < ?;
-            
         """.trimIndent()
 
+    val statsFromTimespanInterval = """
+        SELECT 
+            count(distinct changeset_id) as changesets,
+            count(distinct user_id) as users,
+            sum(road_length) as roads,
+            count(building_area) as buildings,
+            count(*) as edits,
+            toStartOfInterval(fromUnixTimestamp((changeset_timestamp / 1000)::integer), INTERVAL ?) as startdate,
+            toStartOfInterval(fromUnixTimestamp((changeset_timestamp / 1000)::integer), INTERVAL ?) + INTERVAL ? as enddate
+        FROM "stats"
+        WHERE
+            hashtag = ? and changeset_timestamp > ? and changeset_timestamp < ?
+        GROUP BY 
+            startdate
+    """.trimIndent()
 
-    fun getStats(hashtag: String) = create(dataSource)
-        .withHandle<Map<String, Any>, RuntimeException> { asMap(it, "#$hashtag") } + ("hashtag" to hashtag)
+    fun getStats(hashtag: String) = create(dataSource).withHandle<Map<String, Any>, RuntimeException> { asMap(it, "#$hashtag") } + ("hashtag" to hashtag)
 
 
     fun getStatsForTimeSpan(hashtag: String, startDate: Instant?, endDate: Instant?) = create(dataSource)
-        .withHandle<Map<String, Any>, RuntimeException> {
-            asMapFromTimeSpan(it, "#$hashtag", startDate ?: EPOCH, endDate ?: now())
-        } + ("hashtag" to hashtag)
+            .withHandle<Map<String, Any>, RuntimeException> {
+                asMapFromTimeSpan(it, "#$hashtag", startDate ?: EPOCH, endDate ?: now())
+            } + ("hashtag" to hashtag)
+
+    fun getStatsForTimeSpanInterval(hashtag: String, startDate: Instant, endDate: Instant, interval: String) = create(dataSource).withHandle<List<Map<String, Any>>, RuntimeException> {
+            asMapFromTimeSpanInterval(it, "#$hashtag", startDate, endDate,  getGroupbyInterval(interval))
+        }
+
+    fun Duration.toSqlString(): String {
+        val days = toDays()
+        val hours = toHoursPart()
+        val minutes = toMinutesPart()
+        return "INTERVAL '${days}D ${hours}H ${minutes}M'"
+    }
 
 
-    private fun asMap(handle: Handle, hashtag: String) = handle
-        .select(stats, hashtag)
-        .mapToMap()
-        .single()
+    /**
+     * Translates a custom ISO 8601 interval to the corresponding ClickHouse query for aggregator and grouper.
+     *
+     * @param interval The custom ISO 8601 interval string.
+     * @return A tuple containing the aggregator and grouper strings for the ClickHouse query.
+     *         If the interval is not in the expected format, the aggregator and grouper strings are empty.
+     *         ? is this how we want it to be?
+     */
+    fun getGroupbyInterval(interval: String): String {
+        // Regular expression pattern to match the ISO 8601 interval format
+        val pattern = """^P(\d+Y)?(\d+M)?(\d+W)?(\d+D)?(T(\d+H)?(\d+M)?)?$""".toRegex()
+        val matchResult = pattern.find(interval)
 
-    private fun asMapFromTimeSpan(handle: Handle, hashtag: String, startDate: Instant, endDate: Instant) = handle
-        .select(statsFromTimeSpan, hashtag, startDate.toEpochMilli(), endDate.toEpochMilli())
-        .mapToMap()
-            .single()
+        if (matchResult == null) { // maybe throw error instead
+            return ""
+        }
+
+        // Extract the components (years, months, weeks, days, hours, minutes) from the interval
+        val (years, months, weeks, days, hours, minutes) = matchResult.destructured
+
+        // Build the aggregator string based on the extracted components
+        val aggregator = buildString {
+            if (!years.isNullOrEmpty()) {
+                append(years.removeSuffix("Y"))
+                append(" YEAR")
+            } else if (!months.isNullOrEmpty()) {
+                append(months.removeSuffix("M"))
+                append(" MONTH")
+            } else if (!weeks.isNullOrEmpty()) {
+                append(weeks.removeSuffix("W"))
+                append(" WEEK")
+            } else if (!days.isNullOrEmpty()) {
+                append(days.removeSuffix("D"))
+                append(" DAY")
+            } else if (!hours.isNullOrEmpty()) {
+                append(hours.removePrefix("T").removeSuffix("H"))
+                append(" HOUR")
+            } else if (!minutes.isNullOrEmpty()) {
+                append(minutes.removePrefix("T").removeSuffix("M"))
+                append(" MINUTE")
+            }
+
+        }
+
+
+        return aggregator.trim()
+    }
+
+    private fun asMap(handle: Handle, hashtag: String) = handle.select(stats, hashtag).mapToMap().single()
+    private fun asMapFromTimeSpan(handle: Handle, hashtag: String, startDate: Instant, endDate: Instant) = handle.select(statsFromTimeSpan, hashtag, startDate.toEpochMilli(), endDate.toEpochMilli()).mapToMap().single()
+    private fun asMapFromTimeSpanInterval(handle: Handle, hashtag: String, startDate: Instant, endDate: Instant, groupBy: String): List<Map<String, Any>> = handle.select(statsFromTimespanInterval, groupBy , groupBy , groupBy , hashtag, startDate.toEpochMilli(), endDate.toEpochMilli()).mapToMap().list()
 }
