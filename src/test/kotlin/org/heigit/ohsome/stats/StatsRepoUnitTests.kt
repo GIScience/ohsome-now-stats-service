e5991ccd3178efd7b2d0825298ce6b23b9758918
Clemens Langer <clemens.langer@stud.uni-heidelberg.de>
2023-06-22 15:38:55 +0200

    Feature: StatsRepoUnitTest class #7

--- src/test/kotlin/org/heigit/ohsome/stats/StatsRepoUnitTests.kt
+++ src/test/kotlin/org/heigit/ohsome/stats/StatsRepoUnitTests.kt
@@ -2,39 +2,36 @@ package org.heigit.ohsome.stats
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.mockito.Mockito
 
 
+class StatsRepoUnitTests {
 
-class StatsRepoUnitTests{
-        private lateinit var repo: StatsRepo
-        @Test
-        fun `translate ISO Period 'P1W' to Clickhouse interval '1 Week'`(){
-            val result = this.repo.translateISOPeriodToClickhouseInterval("1W")
-            assertThat(result).isEqualTo("1 Week")
-        }
-        @Test
-        fun `translate ISO Period 'P1M' to Clickhouse interval '1 Month'`(){
-            val result = this.repo.translateISOPeriodToClickhouseInterval("1M")
-            assertThat(result).isEqualTo("1 Month")
-        }
-        @Test
-        fun `translate ISO Period 'P1Y' to Clickhouse interval '1 Year'`(){
-            val result = this.repo.translateISOPeriodToClickhouseInterval("1Y")
-            assertThat(result).isEqualTo("1 Year")
-        }
-        @Test
-        fun `translate ISO Period 'P1D' to Clickhouse interval '1 Day'`(){
-            val result = this.repo.translateISOPeriodToClickhouseInterval("1D")
-            assertThat(result).isEqualTo("1 Day")
-        }
-        @Test
-        fun `translate ISO Period 'P1H' to Clickhouse interval '1 Hour'`(){
-            val result = this.repo.translateISOPeriodToClickhouseInterval("1H")
-            assertThat(result).isEqualTo("1 Hour")
-        }
-        @Test
-        fun `translate ISO Period 'PT1M' to Clickhouse interval '1 Minute'`(){
-            val result = this.repo.translateISOPeriodToClickhouseInterval("1M")
-            assertThat(result).isEqualTo("1 Minute")
-        }
+    private val repo = StatsRepo()
+
+
+    @Test
+    fun `translate ISO Period 'P1W' to Clickhouse interval '1 Week'`() {
+        val result = this.repo.getGroupbyInterval("P1W")
+        assertThat(result).isEqualTo("1 WEEK")
+    }
+
+    @Test
+    fun `translate ISO Period 'P1M' to Clickhouse interval '1 Month'`() {
+        val result = this.repo.getGroupbyInterval("P1M")
+        assertThat(result).isEqualTo("1 MONTH")
+    }
+
+    @Test
+    fun `translate ISO Period 'P1M' to Clickhouse interval '1 MINUTE'`() {
+        val result = this.repo.getGroupbyInterval("PT1M")
+        assertThat(result).isEqualTo("1 MINUTE")
+    }
+
+    @Test
+    fun `translate ISO Period 'P1Y' to Clickhouse interval '1 Year'`() {
+        val result = this.repo.getGroupbyInterval("P1Y")
+        assertThat(result).isEqualTo("1 YEAR")
+    }
 }
