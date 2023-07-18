6c58429d14d22d1a2e6f0297cdd1b2585b64a9b8
jstier <stier.jochen@web.de>
2023-07-18 14:29:29 +0200

    feat(hashtag handler) hashtag is converted to lowerstirng

--- src/main/kotlin/org/heigit/ohsome/stats/utils/HashtagHandler.kt
+++ src/main/kotlin/org/heigit/ohsome/stats/utils/HashtagHandler.kt
@@ -1,9 +1,13 @@
 package org.heigit.ohsome.stats.utils
 
+import java.util.*
+
 class HashtagHandler(var hashtag: String, var isWildCard: Boolean = false) {
     init {
+        this.hashtag = this.hashtag.lowercase(Locale.getDefault())
         detectWildCard(hashtag)
     }
+
 
     fun detectWildCard(hashtag: String) {
         isWildCard = hashtag.last() == '*'
