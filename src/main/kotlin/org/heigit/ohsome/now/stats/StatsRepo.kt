01096b4dad6e71866bbfa74405c84ac52edc973a
jstier <stier.jochen@web.de>
2023-09-06 11:39:47 +0200

    fix #42

--- src/main/kotlin/org/heigit/ohsome/now/stats/StatsRepo.kt
+++ src/main/kotlin/org/heigit/ohsome/now/stats/StatsRepo.kt
@@ -60,27 +60,34 @@ class StatsRepo {
     @Suppress("LongMethod")
     //language=sql
     private fun getStatsFromTimeSpanInterval(hashtagHandler: HashtagHandler) = """
-        SELECT
-            count(distinct changeset_id) as changesets,
-            count(distinct user_id) as users,
-            ifNull(sum(road_length_delta)/1000, 0) as roads,
-            ifNull(sum(building_edit), 0) as buildings,
-            count(map_feature_edit) as edits,
-            toStartOfInterval(changeset_timestamp, INTERVAL :interval)::DateTime as startdate,
-            (toStartOfInterval(changeset_timestamp, INTERVAL :interval)::DateTime + INTERVAL :interval) as enddate
-        FROM "stats"
-        WHERE
-            ${if (hashtagHandler.isWildCard) "startsWith" else "equals"}(hashtag, :hashtag)
-            AND changeset_timestamp > parseDateTimeBestEffort(:startdate)
-            AND changeset_timestamp < parseDateTimeBestEffort(:enddate)
-        GROUP BY
-            startdate
-        ORDER BY startdate ASC
-        WITH FILL
-            FROM toStartOfInterval(parseDateTimeBestEffort(:startdate), INTERVAL :interval)::DateTime
-            TO toStartOfInterval(parseDateTimeBestEffort(:enddate), INTERVAL :interval)::DateTime
-        STEP INTERVAL :interval Interpolate (enddate as (startdate + INTERVAL :interval + INTERVAL :interval))
-    """.trimIndent()
+    SELECT
+        count(distinct changeset_id) as changesets,
+        count(distinct user_id) as users,
+        ifNull(sum(road_length_delta)/1000, 0) as roads,
+        ifNull(sum(building_edit), 0) as buildings,
+        count(map_feature_edit) as edits,
+        toStartOfInterval(changeset_timestamp, INTERVAL :interval)::DateTime as startdate,
+        (toStartOfInterval(changeset_timestamp, INTERVAL :interval)::DateTime + INTERVAL :interval) as enddate
+    FROM "stats"
+    WHERE
+        ${if (hashtagHandler.isWildCard) "startsWith" else "equals"}(hashtag, :hashtag)
+        AND changeset_timestamp > parseDateTimeBestEffort(:startdate)
+        AND changeset_timestamp < parseDateTimeBestEffort(:enddate)
+    GROUP BY
+        startdate
+    ORDER BY startdate ASC
+    WITH FILL
+        FROM toStartOfInterval(parseDateTimeBestEffort(:startdate), INTERVAL :interval)::DateTime
+        TO toStartOfInterval(parseDateTimeBestEffort(:enddate), INTERVAL :interval)::DateTime
+    STEP INTERVAL :interval Interpolate (
+        enddate as (
+            if (
+                startdate != parseDateTimeBestEffort('1970-01-01 00:00:00'), -- condition
+                startdate + INTERVAL :interval + INTERVAL :interval, 			 -- then
+                toStartOfInterval(parseDateTimeBestEffort(:startdate), INTERVAL :interval) + INTERVAL :interval -- else
+            )
+	    )
+    )""".trimIndent()
 
     @Suppress("LongMethod")
     //language=sql
