3794952b630bac041c6e8cade83c87df20f4f6ca
Matthias Merdes <matthias.merdes@heigit.org>
2023-11-23 11:45:40 +0100

    test: add column for countries to topic schema and insert countries test data   #47
    ohsome-contributions-stats-service: 'Support queries for Topic stats'
    https://github.com/GIScience/ohsome-contributions-stats-service/issues/47

--- src/test/resources/init_schema_place_view.sql
+++ src/test/resources/init_schema_place_view.sql
@@ -3,6 +3,7 @@ CREATE TABLE IF NOT EXISTS topic_place
     `changeset_timestamp` DateTime,
     `hashtag`             String,
     `user_id`             Int32,
+    `country_iso_a3`      Array(String),
     `place_current`       String,
     `place_before`        String
 
