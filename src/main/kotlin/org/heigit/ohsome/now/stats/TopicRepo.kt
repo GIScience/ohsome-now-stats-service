d02e4e5943a497eb16d1f2c5636bbccec7e55c17
Matthias Merdes <matthias.merdes@heigit.org>
2023-11-27 14:09:48 +0100

    feature: copy repo stats impl to topic impl including tests   #47
    ohsome-contributions-stats-service: 'Support queries for Topic stats'
    https://github.com/GIScience/ohsome-contributions-stats-service/issues/47

--- src/main/kotlin/org/heigit/ohsome/now/stats/TopicRepo.kt
+++ src/main/kotlin/org/heigit/ohsome/now/stats/TopicRepo.kt
@@ -2,6 +2,7 @@ package org.heigit.ohsome.now.stats
 
 import org.heigit.ohsome.now.stats.utils.CountryHandler
 import org.heigit.ohsome.now.stats.utils.HashtagHandler
+import org.heigit.ohsome.now.stats.utils.getGroupbyInterval
 import org.jdbi.v3.core.Handle
 import org.jdbi.v3.core.Jdbi.create
 import org.slf4j.Logger
@@ -21,6 +22,7 @@ class TopicRepo {
     lateinit var dataSource: DataSource
 
     private val logger: Logger = LoggerFactory.getLogger(TopicRepo::class.java)
+
 
     @Suppress("LongMethod")
     //language=sql
@@ -44,6 +46,44 @@ class TopicRepo {
         ;
         """.trimIndent()
 
+
+    @Suppress("LongMethod")
+    //language=sql
+    private fun topicStatsFromTimeSpanIntervalSQL(hashtagHandler: HashtagHandler, countryHandler: CountryHandler) = """
+    SELECT
+        count(distinct changeset_id) as changesets,
+        count(distinct user_id) as users,
+        ifNull(sum(road_length_delta)/1000, 0) as roads,
+        ifNull(sum(building_edit), 0) as buildings,
+        count(map_feature_edit) as edits,
+        toStartOfInterval(changeset_timestamp, INTERVAL :interval)::DateTime as startdate,
+        (toStartOfInterval(changeset_timestamp, INTERVAL :interval)::DateTime + INTERVAL :interval) as enddate
+    FROM "stats"
+    WHERE
+        ${hashtagHandler.variableFilterSQL}(hashtag, :hashtag)
+        AND changeset_timestamp > parseDateTimeBestEffort(:startdate)
+        AND changeset_timestamp < parseDateTimeBestEffort(:enddate)
+        ${countryHandler.optionalFilterSQL}
+    GROUP BY
+        startdate
+    ORDER BY startdate ASC
+    WITH FILL
+        FROM toStartOfInterval(parseDateTimeBestEffort(:startdate), INTERVAL :interval)::DateTime
+        TO toStartOfInterval(parseDateTimeBestEffort(:enddate), INTERVAL :interval)::DateTime
+    STEP INTERVAL :interval Interpolate (
+        enddate as (
+            if (
+                startdate != parseDateTimeBestEffort('1970-01-01 00:00:00'), -- condition
+                ((startdate + INTERVAL :interval) + INTERVAL :interval), 			 -- then
+                (toStartOfInterval(parseDateTimeBestEffort(:startdate), INTERVAL :interval) + INTERVAL :interval) -- else
+            )
+	    )
+    )
+    """.trimIndent()
+
+
+
+
     @Suppress("LongParameterList")
     fun getTopicStatsForTimeSpan(
         hashtagHandler: HashtagHandler,
@@ -65,6 +105,31 @@ class TopicRepo {
 
         return result + ("hashtag" to hashtagHandler.hashtag)
     }
+
+
+    @Suppress("LongParameterList")
+    fun getTopicStatsForTimeSpanInterval(
+        hashtagHandler: HashtagHandler,
+        startDate: Instant?,
+        endDate: Instant?,
+        interval: String,
+        countryHandler: CountryHandler
+    ): List<Map<String, Any>> {
+
+        logger.info("Getting topic stats for hashtag: ${hashtagHandler.hashtag}, startDate: $startDate, endDate: $endDate, interval: $interval")
+
+        return query {
+            it.select(topicStatsFromTimeSpanIntervalSQL(hashtagHandler, countryHandler))
+                .bind("interval", getGroupbyInterval(interval))
+                .bind("startdate", startDate ?: EPOCH)
+                .bind("enddate", endDate ?: now())
+                .bind("hashtag", hashtagHandler.hashtag)
+                .mapToMap()
+                .list()
+        }
+    }
+
+
 
 
     //TODO: remove duplication with StatsRepo
--- src/test/kotlin/org/heigit/ohsome/now/stats/TopicRepoIntegrationTests.kt
+++ src/test/kotlin/org/heigit/ohsome/now/stats/TopicRepoIntegrationTests.kt
@@ -15,6 +15,7 @@ import org.springframework.test.context.jdbc.Sql
 import org.testcontainers.containers.ClickHouseContainer
 import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
+import java.time.Instant
 
 
 @SpringBootTest(webEnvironment = NONE)
@@ -61,6 +62,51 @@ class TopicRepoIntegrationTests {
         println(result)
         assertEquals(expected.toString(), result.toString())
     }
+
+
+    @Test
+    @Sql(*["/init_schema.sql", "/stats_400rows.sql"])
+    fun `getTopicStatsForTimeSpanInterval returns partial data in time span for start and end date with hashtag aggregated by month with 1 country`() {
+        val startDate = Instant.ofEpochSecond(1420991470)
+        val endDate = Instant.ofEpochSecond(1639054890)
+        val hashtagHandler = HashtagHandler("&uganda")
+        val result = this.repo.getTopicStatsForTimeSpanInterval(hashtagHandler, startDate, endDate, "P1M", CountryHandler(listOf("XYZ")))
+        println(result)
+        assertEquals(83, result.size)
+        assertEquals(7, result[0].size)
+        assertEquals("2015-01-01T00:00", result[0]["startdate"].toString())
+        assertEquals("1", result[35]["users"].toString())
+        assertEquals("1", result[35]["changesets"].toString())
+    }
+
+
+    @Test
+    @Sql(*["/init_schema.sql", "/stats_400rows.sql"])
+    fun `getTopicStatsForTimeSpanInterval fills data between two dates with zeros`() {
+        val startDate = Instant.ofEpochSecond(1503644723)
+        val endDate = Instant.ofEpochSecond(1640486233)
+        val hashtagHandler = HashtagHandler("&gid")
+        val result = this.repo.getTopicStatsForTimeSpanInterval(hashtagHandler, startDate, endDate, "P1M", this.emptyListCountryHandler)
+        println(result)
+        assertEquals(52, result.size)
+        assertEquals(7, result[0].size)
+        assertEquals("2017-08-01T00:00", result[0]["startdate"].toString())
+    }
+
+
+    @Test
+    @Sql(*["/init_schema.sql", "/stats_400rows.sql"])
+    fun `getTopicStatsForTimeSpanInterval returns all data when nothing is supplied as startdate`() {
+        val startDate = null
+        val endDate = Instant.ofEpochSecond(1639054888)
+        val hashtagHandler = HashtagHandler("&group")
+        val result = this.repo.getTopicStatsForTimeSpanInterval(hashtagHandler, startDate, endDate, "P1M", this.emptyListCountryHandler)
+        println(result)
+        assertEquals(623, result.size)
+        assertEquals(7, result[0].size)
+        assertEquals("1970-01-01T00:00", result[0]["startdate"].toString())
+    }
+
 
 
 }
