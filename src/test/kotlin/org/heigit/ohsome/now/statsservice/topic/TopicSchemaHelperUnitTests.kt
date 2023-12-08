6f7d2f38b096d26cdbececccc0fea3de08aceae0
Matthias Merdes <matthias.merdes@heigit.org>
2023-12-08 14:23:56 +0100

    test: support topic with 2 keys in schema generation  #47
    ohsome-contributions-stats-service: 'Support queries for Topic stats'
    https://github.com/GIScience/ohsome-contributions-stats-service/issues/47

--- src/test/kotlin/org/heigit/ohsome/now/statsservice/topic/TopicSchemaHelperUnitTests.kt
+++ src/test/kotlin/org/heigit/ohsome/now/statsservice/topic/TopicSchemaHelperUnitTests.kt
@@ -7,39 +7,67 @@ import org.junit.jupiter.api.Test
 class TopicSchemaHelperUnitTests {
 
 
+    private val amenityMatcher = KeyValueMatcher("amenity", listOf("doctors", "clinic")) // values not important here
+    private val healthcareMatcher = KeyValueMatcher("healthcare", listOf("doctors", "clinic")) // values not important here
+
 
     @Test
-    fun `can generate topic table DDL for INT stage`() {
+    fun `can generate topic table DDL for single key and INT stage`() {
 
         val expected = file("create_topic_table_for_1_key")
 
-        val matcher = KeyValueMatcher("amenity", listOf("doctors", "clinic")) // values not important here
-        val definition =  KeyValueTopicDefinition("amenity", listOf(matcher))
+        val definition =  KeyValueTopicDefinition("amenity", listOf(amenityMatcher))
+
+        val sql = createTableDDL(definition)
 
 
-        val key = matcher.key
+        assertThat(sql)
+            .isEqualToNormalizingPunctuationAndWhitespace(expected)
+    }
 
 
-        val sql =
-        """
+    @Test
+    fun `can generate topic table DDL for two keys and INT stage`() {
+
+        val expected = file("create_topic_table_for_2_keys")
+
+        val definition =  KeyValueTopicDefinition("healthcare", listOf(healthcareMatcher, amenityMatcher))
+
+        val sql = createTableDDL(definition)
+
+        assertThat(sql)
+            .isEqualToNormalizingPunctuationAndWhitespace(expected)
+    }
+
+
+// TODO:  clean up
+    fun createTableDDL(definition: KeyValueTopicDefinition): String {
+
+
+    val keyColumns = definition.matchers
+        .map(KeyValueMatcher::key)
+        .map(::columnDefinitions)
+        .joinToString(separator = ",\n")
+
+
+    return """
             CREATE TABLE IF NOT EXISTS int.topic_${definition.topic}
             (
                 `changeset_timestamp` DateTime,
                 `hashtag`             String,
                 `user_id`             Int32,
                 `country_iso_a3`      Array(String),
-                `${key}_current`      String,
-                `${key}_before`       String
+                $keyColumns
             )
                 ENGINE = MergeTree
                 PRIMARY KEY( hashtag, changeset_timestamp)
             ;
         """.trimIndent()
-
-
-        assertThat(sql)
-            .isEqualToNormalizingPunctuationAndWhitespace(expected)
     }
+
+    private fun columnDefinitions(key: String) = """
+            `${key}_current`      String, 
+            `${key}_before`       String"""
 
 
 }
---
+++ src/test/resources/expected_sql/create_topic_table_for_2_keys.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS int.topic_healthcare
+(
+    `changeset_timestamp` DateTime,
+    `hashtag`             String,
+    `user_id`             Int32,
+    `country_iso_a3`      Array(String),
+    `healthcare_current`  String,
+    `healthcare_before`   String,
+    `amenity_current`     String,
+    `amenity_before`      String
+)
+    ENGINE = MergeTree
+    PRIMARY KEY( hashtag, changeset_timestamp)
+;
