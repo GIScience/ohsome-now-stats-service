ad247ae58df581c2c4c55363085805028c6c438b
Matthias Merdes <matthias.merdes@heigit.org>
2023-12-08 15:45:56 +0100

    feature: support generation of DDL for materialized view    #47
    ohsome-contributions-stats-service: 'Support queries for Topic stats'
    https://github.com/GIScience/ohsome-contributions-stats-service/issues/47

--- src/main/kotlin/org/heigit/ohsome/now/statsservice/topic/TopicSchemaHelper.kt
+++ src/main/kotlin/org/heigit/ohsome/now/statsservice/topic/TopicSchemaHelper.kt
@@ -1,6 +1,36 @@
 package org.heigit.ohsome.now.statsservice.topic
 
 
+fun createMVDDL(definition: TopicDefinition): String {
+
+
+    val keyColumns = definition
+        .keys()
+        .map(::columnNames)
+        .joinToString(separator = ",\n")
+
+    val whereClause = definition
+        .keys()
+        .map(::whereClauseParts)
+        .joinToString(separator = ",\n")
+
+
+    return """
+        CREATE MATERIALIZED VIEW int.mv__stats_to_topic_${definition.topicName} TO int.topic_${definition.topicName}
+        AS SELECT
+        (
+            `changeset_timestamp`,
+            `hashtag`,
+            `user_id`,
+            `country_iso_a3`,
+            $keyColumns
+        )
+        FROM int.stats
+        WHERE
+            $whereClause
+            """.trimIndent().trimMargin()
+}
+
 fun createTableDDL(definition: TopicDefinition): String {
 
 
@@ -28,6 +58,16 @@ fun createTableDDL(definition: TopicDefinition): String {
 private fun columnDefinitions(key: String) = """
         `${key}_current`      String, 
         `${key}_before`       String"""
+
+
+private fun columnNames(key: String) = """
+        tags['${key}'] as  `${key}_current`, 
+        tags_before['${key}'] as `${key}_before`"""
+
+
+private fun whereClauseParts(key: String) = """
+        tags['${key}']  != '' OR tags_before['${key}'] != '' """
+
 
 
 
--- src/test/kotlin/org/heigit/ohsome/now/statsservice/topic/TopicSchemaHelperUnitTests.kt
+++ src/test/kotlin/org/heigit/ohsome/now/statsservice/topic/TopicSchemaHelperUnitTests.kt
@@ -11,8 +11,23 @@ class TopicSchemaHelperUnitTests {
     private val healthcareMatcher = KeyValueMatcher("healthcare", listOf("doctors", "clinic")) // values not important here
 
 
-    private val expected1Key = file("create_topic_table_for_1_key")
-    private val expected2Keys = file("create_topic_table_for_2_keys")
+    private val expectedTable1Key = file("create_topic_table_for_1_key")
+    private val expectedTable2Keys = file("create_topic_table_for_2_keys")
+
+    private val expectedMV1Key = file("create_topic_mv_for_1_key")
+
+
+
+
+    @Test
+    fun `can generate topic MV DDL for single key and INT stage - key-value definition`() {
+
+        val definition =  KeyValueTopicDefinition("amenity", listOf(amenityMatcher))
+
+        val sql = createMVDDL(definition)
+        assertThat(sql)
+            .isEqualToNormalizingPunctuationAndWhitespace(expectedMV1Key)
+    }
 
 
     @Test
@@ -22,7 +37,7 @@ class TopicSchemaHelperUnitTests {
 
         val sql = createTableDDL(definition)
         assertThat(sql)
-            .isEqualToNormalizingPunctuationAndWhitespace(expected1Key)
+            .isEqualToNormalizingPunctuationAndWhitespace(expectedTable1Key)
     }
 
 
@@ -33,7 +48,7 @@ class TopicSchemaHelperUnitTests {
 
         val sql = createTableDDL(definition)
         assertThat(sql)
-            .isEqualToNormalizingPunctuationAndWhitespace(expected1Key)
+            .isEqualToNormalizingPunctuationAndWhitespace(expectedTable1Key)
     }
 
 
@@ -44,7 +59,7 @@ class TopicSchemaHelperUnitTests {
 
         val sql = createTableDDL(definition)
         assertThat(sql)
-            .isEqualToNormalizingPunctuationAndWhitespace(expected2Keys)
+            .isEqualToNormalizingPunctuationAndWhitespace(expectedTable2Keys)
     }
 
 
