5b3ce580e29e5b53b28e1bb0b610f90823f76b69
Matthias Merdes <matthias.merdes@heigit.org>
2024-01-25 17:00:15 +0100

    feature: introduce caching for first service method  #53
    ohsome-contributions-stats-service: 'Evaluate caching certain requests '
    https://github.com/GIScience/ohsome-contributions-stats-service/issues/53

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,6 +35,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-jdbc")
+    implementation("org.springframework.boot:spring-boot-starter-cache")
 
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.0.4")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
--- src/main/kotlin/org/heigit/ohsome/now/statsservice/Application.kt
+++ src/main/kotlin/org/heigit/ohsome/now/statsservice/Application.kt
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition
 import io.swagger.v3.oas.annotations.info.Info
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cache.annotation.EnableCaching
 
 
 @OpenAPIDefinition(
@@ -13,6 +14,7 @@ import org.springframework.boot.runApplication
         version = "v1"
     )
 )
+@EnableCaching
 @SpringBootApplication
 class Application
 
