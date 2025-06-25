5a55e1ad0147a9858f5401ffc2588ce42b1f0ba8
jstier <stier.jochen@web.de>
2025-06-25 13:07:41 +0200

    [Gradle Release Plugin] - pre tag commit:  '1.4.1'.

--- src/main/kotlin/org/heigit/ohsome/now/statsservice/topic/TopicRepo.kt
+++ src/main/kotlin/org/heigit/ohsome/now/statsservice/topic/TopicRepo.kt
@@ -167,7 +167,7 @@ class TopicRepo {
             ${hashtagHandler.optionalFilterSQL}
             changeset_timestamp > parseDateTimeBestEffort(:startDate)
             AND changeset_timestamp < parseDateTimeBestEffort(:endDate)
-            AND isNotNull(h3_r:resolution) 
+            AND isNotNull(h3_r:resolution)
             ${countryHandler.optionalFilterSQL}
         GROUP BY hex
         FORMAT CSV
